25de326b460a3d8776a03f9b43a0b325c56f18ed
Arpit <arpit.mishra.out@gmail.com>
2024-05-25 18:35:23 +0530

    Added responsivness so menu btn appears

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 `use strict`;
 
 // ********************************
+
+// **********************************
+// **********************************
+let widthLimit = 768;
+let windowWidth = window.innerWidth; // will give innerWidth of window
+
+// **********************************
+// **********************************
 // Making the responsive navigation bar
 
-const requestBtn = document.querySelector(`.request-btn`);
+const requestBtn = document.getElementById(`btn-request`);
 const links = document.querySelector(`.links`);
 const menuBtn = document.querySelector(`.menu-btn`);
+const closeBtn = document.querySelector(`.menu-close`);
