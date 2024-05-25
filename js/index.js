5ba6c24e7e1a2d5c613aa78bb540589d39f30dce
Arpit <arpit.mishra.out@gmail.com>
2024-05-25 16:48:31 +0530

    Added responsivness to the hero section

---
+++ js/index.js
@@ -0,0 +1,8 @@
+`use strict`;
+
+// ********************************
+// Making the responsive navigation bar
+
+const requestBtn = document.querySelector(`.request-btn`);
+const links = document.querySelector(`.links`);
+const menuBtn = document.querySelector(`.menu-btn`);
