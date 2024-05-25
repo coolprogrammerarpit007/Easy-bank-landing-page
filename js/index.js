cc33b25df578aff3190ad68f15140d355bc40429
Arpit <arpit.mishra.out@gmail.com>
2024-05-25 18:58:32 +0530

    Added the responsive navigation bar to the easy-bank landing page

--- js/index.js
+++ js/index.js
@@ -15,3 +15,19 @@ const requestBtn = document.getElementById(`btn-request`);
 const links = document.querySelector(`.links`);
 const menuBtn = document.querySelector(`.menu-btn`);
 const closeBtn = document.querySelector(`.menu-close`);
+
+const modalMenu = document.querySelector(`.modal-menu`);
+// Adding event to the menu btn
+
+menuBtn.addEventListener(`click`, function (e) {
+  menuBtn.classList.add(`hidden`);
+  closeBtn.classList.remove(`hidden`);
+  modalMenu.classList.remove(`hidden`);
+});
+// Adding event to the close btn
+
+closeBtn.addEventListener(`click`, function (e) {
+  closeBtn.classList.add(`hidden`);
+  menuBtn.classList.remove(`hidden`);
+  modalMenu.classList.add(`hidden`);
+});
